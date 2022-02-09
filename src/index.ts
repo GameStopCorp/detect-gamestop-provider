2974c9b9a4ce2f5dce0e5f57149db6ca6623197d
Jeffery Walsh <rlxrlps@gmail.com>
2022-02-08 20:55:12 -0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+interface Provider {
+  isGme?: boolean;
+}
+
+declare global {
+  interface Window {
+    ethereum?: Provider;
+    gme?: Provider;
+  }
+}
+
+interface DetectOpts {
+  timeout: number;
+}
+const detectGmeProvider = (
+  opts: DetectOpts = { timeout: 3000 }
+): Promise<unknown> => {
+  return new Promise((resolve) => {
+    console.log("yo");
+    if (window.gme?.isGme) {
+      resolve(window.gme);
+      return;
+    }
+
+    if (window.ethereum?.isGme) {
+      resolve(window.ethereum);
+      return;
+    }
+
+    let handled;
+
+    const handle = () => {
+      if (!handled) return;
+      handled = true;
+      window.removeEventListener("ethereum#initialized", handle);
+
+      if (window.gme && window.gme.isGme) {
+        resolve(window.gme);
+        return;
+      }
+
+      if (window.ethereum?.isGme) {
+        resolve(window.ethereum);
+        return;
+      }
+
+      console.error("GME Browser Wallet not detected");
+      resolve(null);
+    };
+
+    window.addEventListener("ethereum#initialized", handle);
+
+    setTimeout(handle, opts.timeout);
+  });
+};
+
+export default detectGmeProvider;
